eae8fd5ca643329fa670aa3074ca4a9223b9f7f6
shj312 <sshj312@naver.com>
2020-10-08 16:05:06 +0900

    06_3_2

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
- void square ( int a)
- 
+ int square ( int a)
  {
- 	a=a*a;
+ 	return (a*a);
  }
  
  int main()
  {
  	int a = 2;
- 	square(a);
+ 	a = square(a);
  	printf("a=%i \n",a);
  }
