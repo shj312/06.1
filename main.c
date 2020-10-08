87a8db3a260f64930b4ba71722061d3ac0ccbad4
shj312 <sshj312@naver.com>
2020-10-08 15:50:17 +0900

    06.2

--- main.c
+++ main.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
-void print_star()
-{
-	int i;
-	for(i=0;i<10;i++)
-	 printf("*");
-}
+ 
+ int sumTwo(int a, int b)
+ {
+ 	return (a+b);
+ }
+ 
+ int square(int n)
+ {
+ 	return (n*n);
+ }
+ 
+ int getMax(int x, int y)
+ {
+ 	if (x>y)
+	  return x;
+	else
+	  return y;
+ }
+ 
+ int main(int argc, char *argv[]) {
+ 	
+ 	printf("sumTwo : %i \n", sumTwo(1,2));
+ 	printf("square : %i \n", square(5));
+ 	printf("getMax : %i \n", getMax(10,8));
+ 	
+ 	return 0;
+ }
 
-int main(void)
-{
-	print_star();
-	print_star();
-	print_star();
-
-	 return 0; 
-}
