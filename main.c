0df477bd5ad80504a8d2e91d8e06a9668ad23eaf
shj312 <sshj312@naver.com>
2020-10-08 16:03:49 +0900

    06_3_1

--- main.c
+++ main.c
@@ -1,29 +1,13 @@
 #include <stdio.h>
+ void square ( int a)
  
- int sumTwo(int a, int b)
  {
- 	return (a+b);
+ 	a=a*a;
  }
  
- int square(int n)
+ int main()
  {
- 	return (n*n);
+ 	int a = 2;
+ 	square(a);
+ 	printf("a=%i \n",a);
  }
- 
- int getMax(int x, int y)
- {
- 	if (x>y)
-	  return x;
-	else
-	  return y;
- }
- 
- int main(int argc, char *argv[]) {
- 	
- 	printf("sumTwo : %i \n", sumTwo(1,2));
- 	printf("square : %i \n", square(5));
- 	printf("getMax : %i \n", getMax(10,8));
- 	
- 	return 0;
- }
-
